feec174a2e9a8589498866512954bf5a6cbf03f2
patrickeidrian <patrickeidrian1@gmail.com>
2022-06-22 15:47:18 -0300

    atualizando o contador

--- Desafio-Js/desafio-Contador/contador.js
+++ Desafio-Js/desafio-Contador/contador.js
@@ -1,1 +1,19 @@
+let currentNumberWrapper = document.getElementById("currentNumber");
+let currentNumber = 0;
 
+function increment() {
+    currentNumber += 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+
+function decrement() {
+    currentNumber -= 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+let botaoMenos = document.getElementsByName("subtrair");
+let botaoMais = document.getElementsByName("adicionar");
+
+botaoMenos.addEventListener("click", increment);
+botaoMais.addEventListener("click", decrement);
